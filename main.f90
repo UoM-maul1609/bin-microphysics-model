d7b81385b185c6865efe48c4e9fd0867261020f0
Paul Connolly <p.connolly@manchester.ac.uk>
2022-02-22 15:58:50 +0000

    Squashed 'osnf/' changes from ae0a716..aa0b6a2
    aa0b6a2 added routines to test mode-1 fragmentation
    
    git-subtree-dir: osnf
    git-subtree-split: aa0b6a21076759aa1b2b7a8c7d956a330a644cef

--- main.f90
+++ main.f90
@@ -12,9 +12,10 @@
     real(wp), parameter :: lf=333550._wp, ttr=273.15_wp, cw=4200._wp, &
                 gamma_liq=0.072_wp, DEcrit=0.2_wp, alphar=2.5_wp, alphai=2.5_wp, &
                 pi=4.0_wp*atan(1.0_wp), cr=pi/6.0_wp*1000.0_wp, dr=3.0_wp, ar=836.0_wp, &
-                br=0.8_wp, ci=0.04_wp, di=2.0_wp, ai=8.97_wp, bi=0.42_wp
+                br=0.8_wp, ci=0.04_wp, di=2.0_wp, ai=8.97_wp, bi=0.42_wp, &
+                oneoversix=1._wp/6._wp, dtt=10.e-6_wp, rhow=1000._wp, rhoice=920._wp
     real(wp) :: phi, T, f, nr1, qr, ni, qi, n0r, lambda0r, n0i, lambda0i, &
-        mrthresh, mrupper, miupper, vx, vy
+        mrthresh, mrupper, miupper, vx, vy, lam_freeze, n0_freeze
     
     contains            
         
@@ -93,6 +94,138 @@
         dintegral=dintegral*nfrag_freeze2
     
     end function dintegral
+    
+    function dintegral2(x,y)
+        use numerics_type, only : wp
+        implicit none
+        real(wp), intent(in) :: x
+        real(wp), dimension(:), intent(in) :: y
+        real(wp), dimension(size(y)) :: dintegral2
+        real(wp) :: diamr, mr, vr, n,nt,nb,mb,mt
+        real(wp), dimension(size(y)) :: mi, diami, delv, vi
+        integer(i4b) :: i
+
+        mr=x
+        mi=y
+        diamr=(mr/cr)**(1.0_wp/dr)
+        diami=(mi/ci)**(1.0_wp/di)
+        ! fall-speeds
+        vr=ar*diamr**br
+        vi=ai*diami**bi
+        delv=abs(vr-vi)
+        !delv=max((vx+vy)/8.0,abs(vx-vy))
+        ! last bit is to convert to integral over m
+        dintegral2=pi/4.0_wp*(diamr+diami)**2* &
+            delv*n0r*diamr**alphar* &
+            exp(-lambda0r*diamr)*n0i*diami**alphai*exp(-lambda0i*diami)* &
+            (diamr**(1.0_wp-dr)) / (cr*dr)*(diami**(1.0_wp-di)) / (ci*di)
+        
+        do i=1,size(y)
+            call calculate_mode1(mr,mi(i),260._wp,n,nt,nb,mb,mt)
+            dintegral2(i)=dintegral2(i)*n
+        enddo
+        
+    
+    end function dintegral2
+    
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! mode 1 fragmentation integral over size distribution                         !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!>@author
+	!>Paul J. Connolly, The University of Manchester
+	!>@brief
+	!>calculates the number of fragments and their mass
+    function integral_m1(x)
+        use numerics_type, only : wp, i4b
+        implicit none
+		real(wp), dimension(:), intent(in) :: x
+		real(wp), dimension(size(x)) :: integral_m1
+		
+		real(wp), dimension(size(x)) :: nfrag
+		real(wp) :: n,nt,nb,mb,mt
+		integer(i4b) :: i
+		
+		do i=1,size(x)
+		    call calculate_mode1(x(i),0._wp,260._wp,n,nt,nb,mb,mt)
+		    nfrag(i)=n
+		enddo
+		
+    
+        integral_m1=n0_freeze*exp(-lam_freeze*x)*x**alphar !*nfrag
+    end function integral_m1
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! mode 1 fragmentation                                                         !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!>@author
+	!>Paul J. Connolly, The University of Manchester
+	!>@brief
+	!>calculates the number of fragments and their mass
+    subroutine calculate_mode1(min1,min2,t,n,nt,nb,mb,mt)
+        use numerics_type
+        implicit none
+        real(wp), intent(in) :: min1,min2,t
+        real(wp), intent(inout) :: n, nt, nb, mb, mt
+        real(wp) :: tc, dthresh, x, beta1,log10zeta, log10nabla, t0, zetab, nablab, tb0, &
+            sigma, omega, m,d, fac1
+        
+        if((min2>min1).or.(min1<=6.55e-11_wp)) then
+            ! the ice is more massive than the drop or drop small, don't do it
+            n=0._wp
+            nt=0._wp
+            nb=0._wp
+            return
+        endif
+    
+        d = (6._wp*min1/rhow)**(1._wp/3._wp)
+        tc=t-ttr
+        dthresh = min(d,1.6e-3)
+        x = log10(dthresh*1000._wp)
+        
+        ! table 3, phillips et al.
+        beta1 = 0.
+        log10zeta = 2.4268_wp*x*x*x + 3.3274_wp*x*x + 2.0783_wp*x + 1.2927_wp
+        log10nabla = 0.1242_wp*x*x*x - 0.2316_wp*x*x - 0.9874_wp*x - 0.0827_wp
+        t0 = -1.3999_wp*x*x*x - 5.3285_wp*x*x - 3.9847_wp*x - 15.0332_wp
+        
+        ! table 4, phillips et al. 
+        zetab = -0.4651_wp*x*x*x - 1.1072_wp*x*x - 0.4539_wp*x+0.5137_wp
+        nablab = 28.5888*x*x*x + 49.8504_wp*x*x + 22.4873_wp*x + 8.0481_wp
+        tb0 = 13.3588_wp*x*x*x + 15.7432_wp*x*x - 2.6545_wp*x - 18.4875_wp
+        
+        sigma = min(max((d-50.e-6_wp)/10.e-6_wp,0._wp), 1._wp)
+        omega = min(max((-3._wp-tc)/3._wp,0._wp),1._wp)
+        
+        n = sigma*omega*(10._wp**log10zeta *(10**log10nabla)**2) / &
+            ((tc-t0)**2+(10._wp*log10nabla)**2+beta1*tc)
+        
+        ! total number of fragments
+        n=n*d/dthresh
+        ! number of large fragments
+        nb = min(sigma*omega*(zetab*nablab**2/((tc-tb0)**2+nablab**2)),n)
+        ! number of small fragments
+        nt = n-nb
+        
+        m=oneoversix*rhow*pi*d**3
+        
+        ! mass of large fragments
+        mb=0.4_wp*m
+        
+        ! mass of small fragments
+        mt=oneoversix*rhoice*pi*dtt**3
+        
+        fac1=min((mt*nt+mb*nb)/min1,1._wp)
+        nt = nt *fac1
+        nb = nb *fac1
+        n=nt+nb
+        
+    end subroutine calculate_mode1
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    
+    
 
     function limit1(x)
         use numerics_type, only : wp
@@ -109,6 +242,23 @@
         real(wp) :: limit2
         limit2=miupper
     end function limit2
+    
+    function limit3(x)
+        use numerics_type, only : wp
+        implicit none
+        real(wp), intent(in) :: x
+        real(wp) :: limit3
+        limit3=mrthresh
+    end function limit3
+!
+    function limit4(x)
+        use numerics_type, only : wp
+        implicit none
+        real(wp), intent(in) :: x
+        real(wp) :: limit4
+        limit4=x
+    end function limit4
+    
     
     end module test
     
@@ -130,13 +280,14 @@
         use numerics_type
         use numerics, only : dvode, dfsid1, zeroin,fmin, vode_integrate, &
                 tridiagonal, erfinv, invgammainc, quad2d_qgaus, &
-                quad2d_romb, gammainc_scal, gammainc
+                quad2d_romb, romb, gammainc_scal, gammainc
         use random, only : random_normal
         use hypergeo, only : hygfx
         use test, only : phi, ni, qi, nr1, qr, ttr, vx,vy, lf, &
             miupper, mrupper, mrthresh, ar, ai, br, bi, cr, ci, dr, di, &
             n0i, n0r, lambda0r, lambda0i, alphar, alphai, cw, f,&
-            limit1, limit2, dintegral, calculate_parameters
+            limit1, limit2, limit3, limit4, dintegral, calculate_parameters, &
+            dintegral2, integral_m1, lam_freeze, n0_freeze
         implicit none
         real(wp) :: machep
         real(wp) :: ax,bx,tol,f1,f2,func,h0,err
@@ -274,9 +425,9 @@
         qr=2.e-5_wp
         ni=.001e3_wp
         qi=0.1e-3_wp
-        mrthresh = cr*150.e-6_wp**dr
-        mrupper=cr*1.e-3_wp**dr
-        miupper=ci*5.e-3_wp**di
+        mrthresh = cr*150.e-6_wp**dr    ! mass of rain with D>150 microns
+        mrupper=cr*1.e-3_wp**dr         ! mass of rain with D>1 mm
+        miupper=ci*5.e-3_wp**di         ! mass of ice with D>5 mm
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
     
@@ -292,9 +443,9 @@
         p=invgammainc(p,alphar+1.0_wp)
 !         p=14.938751954612494_wp
 !         print *,p,gammainc(alphar+1.0,p), alphar
-        mrupper=cr*(p/lambda0r)**dr
-        miupper=ci*(p/lambda0i)**di  
-        mrupper=min(mrupper,miupper)  
+        mrupper=cr*(p/lambda0r)**dr     ! mass of rain that is larger than 99.99% of dist
+        miupper=ci*(p/lambda0i)**di     ! mass of ice that is larger than 99.99% of dist
+        mrupper=min(mrupper,miupper)    ! min of two
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
     
@@ -310,6 +461,9 @@
     !         ci*n0i*gamma(1.+alphai+di)/lambda0i**(1.+alphai+di)
         ss=0._wp
     !     print *,cr*(p/lambda0r)**dr,ci*(p/lambda0i)**di,mrthresh
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! for mode-2
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         if(mrupper.gt.mrthresh) then
             call quad2d_romb(dintegral,limit1,limit2,x1,x2,ss)
         endif
@@ -321,6 +475,47 @@
         endif
         
         print *,'Double Integral solution 2 is: ', ss   
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! for mode-1
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        mrthresh = cr*1.e-6_wp**dr    ! mass of rain with D>1 microns
+        x1=mrthresh ! can happen for all sizes, so pick a small one
+        x2=mrupper
+        if(mrupper.gt.mrthresh) then
+            call quad2d_romb(dintegral2,limit3,limit4,x1,x2,ss)
+        endif
+        
+        print *,'Double Integral solution is: ', ss, miupper,gammainc(2.0_wp,0.99_wp)        
+        ss=0._wp
+        if(mrupper.gt.mrthresh) then
+            call quad2d_qgaus(dintegral2,limit3,limit4,x1,x2,ss)
+        endif
+        
+        print *,'Double Integral solution 2 is: ', ss   
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        
+        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! for mode-1 - freezing, no collisions
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        mrthresh = cr*1.e-6_wp**dr    ! mass of rain with D>1 microns
+        x1=mrthresh ! can happen for all sizes, so pick a small one
+        x2=mrupper
+        lam_freeze=lambda0r
+        n0_freeze=n0r
+        if(mrupper.gt.mrthresh) then
+            ss=romb(integral_m1,x1,x2)
+        endif
+        
+        print *,'Integral solution is: ', ss,x1,x2      
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        
+        
+        
         
     end program main
 
