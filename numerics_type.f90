bff437104979324ab8aa65607db8076eb7425dc4
Paul Connolly <p.connolly@manchester.ac.uk>
2023-09-12 12:24:31 +0100

    Squashed 'osnf/' changes from 2a418d2..ec53c06
    ec53c06 bug fix for reverse direction integration with vode_integrate
    b9776df remove warning for ode solver
    060f0c0 added twopi
    
    git-subtree-dir: osnf
    git-subtree-split: ec53c06ad17b54561666e387e74fd54f936b7198

--- numerics_type.f90
+++ numerics_type.f90
@@ -17,4 +17,5 @@ module numerics_type
 	integer, parameter :: spc = kind((1.0,1.0))
 	integer, parameter :: dpc = kind((1.0d0,1.0d0))
     real(wp), parameter :: pi=3.141592653589793238462643383279502884197_wp
+    real(wp), parameter :: twopi=2.*wp*pi
 end module numerics_type
